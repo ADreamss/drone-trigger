47dcce2f187efa843658d164d7c8ad6276071fd9
Adrian Fernandez <adrian.fernandez@xtime.com>
2017-09-20 17:26:53 -0400

    Added comments

--- wrapper.sh
+++ wrapper.sh
@@ -3,10 +3,16 @@
 export DRONE_SERVER=${PLUGIN_SERVER}
 [ -n "$DOWNSTREAM_TOKEN" ] && export PLUGIN_TOKEN=${DOWNSTREAM_TOKEN}
 export DRONE_TOKEN=${PLUGIN_TOKEN}
+echo 'nameserver 8.8.8.8' > /etc/resolv.conf
+
+echo "Exports done"
 
 if [[ $# == '0' ]]; then
 	nBuild=$(drone build last --branch ${PLUGIN_BRANCH} --format {{.Number}} ${PLUGIN_REPOSITORY})
-	status=$(drone build last --branch ${PLUGIN_BRANCH} --format {{.Status}} ${PLUGIN_REPOSITORY})
+	echo ${PLUGIN_REPOSITORY}
+	echo ${PLUGIN_BRANCH}
+	echo $nBuild
+	
 	if [[ ${PLUGIN_FORK} = "true" ]]; then
 		echo "New build"
 		drone build start --fork ${PLUGIN_REPOSITORY} $nBuild
